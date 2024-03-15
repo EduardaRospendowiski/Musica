a1f0e0066e593714ff1b5e35a926bb683858e36d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 20:08:42 -0300

    Adição de codigo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"Garota de Ipanema");
+Console.ResetColor();
+Console.WriteLine(@"
+Olha que coisa mais linda, mais cheia de graça
+É ela, menina, que vem e que passa
+Num doce balanço a caminho do mar
+
+Moça do corpo dourado, do Sol de Ipanema
+O seu balançado é mais que um poema
+É a coisa mais linda que eu já vi passar
+
+Ah, por que estou tão sozinho?
+Ah, por que tudo é tão triste?
+Ah, a beleza que existe
+A beleza que não é só minha
+Que também passa sozinha");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"Ah, se ela soubesse que quando ela passa
+O mundo inteirinho se enche de graça
+E fica mais lindo por causa do amor");
+Console.ResetColor();
+Console.WriteLine(@"Ah, por que estou tão sozinho?
+Ah, por que tudo é tão triste?
+Ah, a beleza que existe
+A beleza que não é só minha
+Que também passa sozinha
+
+Ah, se ela soubesse que quando ela passa
+O mundo inteirinho se enche de graça
+E fica mais lindo por causa do amor
+Por causa do amor
+Por causa do amor");
